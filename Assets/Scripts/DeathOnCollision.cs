bfd530a1e624dbf9e638e796e14fe5244d308678
Vladimir Moroz <vovanmoroz1509@gmail.com>
2018-03-04 18:59:58 +0200

    Created death animation, but couldn't get it to work yet

--- Assets/Scripts/DeathOnCollision.cs
+++ Assets/Scripts/DeathOnCollision.cs
@@ -2,31 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class DeathOnCollision : MonoBehaviour {
-    // Use this for initialization
+
+    
     public GameObject Door;
+   
+
     void Start () {
         Door = GameObject.FindGameObjectWithTag("Door");
-	}
+        
+    }
 
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+    }
     void OnCollisionEnter2D (Collision2D other)
     {
+        
         if (other.gameObject.tag == "Goo")
         {
-            Debug.Log("you died");
+            
+            Debug.Log("you died");            
             Application.LoadLevel("demo");
-            Time.timeScale = 1f;    //Resumes normal time in case character dies during time slowdown
+
         }
         if(other.gameObject.tag == "PickUp")
             {
             Destroy(other.gameObject);
             Destroy(Door);
-            }
+        }
 
+        
     }
 }
