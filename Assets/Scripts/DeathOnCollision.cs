189dadcb4be6b9f87732da9850eb74770db6e7db
Jamie Yoon <rkawk3316@gmail.com>
2018-03-05 13:03:25 +0200

    Minor Animation Changes

--- Assets/Scripts/DeathOnCollision.cs
+++ Assets/Scripts/DeathOnCollision.cs
@@ -8,17 +8,17 @@ public class DeathOnCollision : MonoBehaviour {
 
     
     public GameObject Door;
-   
+    Animator anim;
+    int death = Animator.StringToHash("deathanim");
 
     void Start () {
         Door = GameObject.FindGameObjectWithTag("Door");
-        
+        anim = GetComponent<Animator>();
     }
 
 	
 	// Update is called once per frame
 	void Update () {
-        
     }
     void OnCollisionEnter2D (Collision2D other)
     {
@@ -26,9 +26,9 @@ public class DeathOnCollision : MonoBehaviour {
         if (other.gameObject.tag == "Goo")
         {
             
-            Debug.Log("you died");            
+            Debug.Log("you died");
+            anim.SetBool("dead", true);
             Application.LoadLevel("demo");
-
         }
         if(other.gameObject.tag == "PickUp")
             {
